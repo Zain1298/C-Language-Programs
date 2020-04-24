6391b2affe1510c39770abcb3e51f4ef304f3377
Syed Zain Rashid <55804149+Zain1298@users.noreply.github.com>
2020-04-25 00:41:22 +0500

    How is a day today ?

---
+++ If-else.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+	int temp;
+	printf("Please type in the temperature : ");
+	scanf("%d",&temp);
+	if (temp , 80)
+	if (temp > 60)
+	printf("Nice day!");
+	else 
+	printf("Sure is chilly!");
+	else 
+	printf("Sure is hot!");
+}
